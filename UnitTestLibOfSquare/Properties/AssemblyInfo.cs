f72af05f57ffd964493f122a042ab0fafff675f5
Akinok <nuqpa1@mail.ru>
2021-05-06 10:52:31 +0300

    Добавьте файлы проекта.

---
+++ UnitTestLibOfSquare/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestLibOfSquare")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestLibOfSquare")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("692a44a2-5d77-4a80-8389-e75e4850512d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestLibOfSquare/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibOfSquare;
+using System;
+
+namespace UnitTestLibOfSquare
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TriangleIsRectangular()
+        {
+            int a = 3;
+            int b = 4;
+            int c = 5;
+            Triangle myTriangle = new Triangle(a, b, c);
+            myTriangle.CheckOfTriangle();
+        }
+        [TestMethod]
+        public void LargeNumbersOfCircle()
+        {
+            int a = 1000;
+            int b = 2000;
+            int c = 3000;
+            Triangle myTriangle = new Triangle(a, b, c);
+            Square square = new Square();
+            square.SquareOfCircleFromRadius(100000);
+            square.SquareOfCircleFromTriangle(myTriangle);
+        }
+        [TestMethod]
+        public void NegativeValues() 
+        {
+            int a = -1000;
+            int b = -2000;
+            int c = -3000;
+            Triangle myTriangle = new Triangle(a, b, c);
+            Square square = new Square();
+            square.SquareOfCircleFromRadius(-100000);
+            square.SquareOfCircleFromTriangle(myTriangle);
+        }
+    }
+}
